38289fea6210a80b8ec0fefbb7758c89396e7dfd
brianpocasangre <brianpocasangre1@gmail.com>
2024-05-16 14:52:59 -0700

    make calculator operations/functions work

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
-let num1;
-let num2;
+let num1 = '';
+let num2 = '';
 let operator = '';
 
 function add(num1, num2) {
@@ -15,6 +15,57 @@ function divide(num1, num2) {
   return num1 / num2;
 }
 
-function operate(num1, num2, operator) {
-  return operator(num1, num2);
+const display = document.querySelector('.display');
+const buttons = Array.from(document.querySelectorAll('.pad'));
+const operators = Array.from(document.querySelectorAll('.operate'));
+
+function populate(e) {
+  if (num1 !== '' && operator !== '' && this.id !== 'equals') {
+    num2 += this.id;
+    display.innerHTML = num2;
+  } else if (
+    this.id !== 'add' &&
+    this.id !== 'subtract' &&
+    this.id !== 'multiply' &&
+    this.id !== 'divide' &&
+    this.id !== 'equals'
+  ) {
+    num1 += this.id;
+    display.innerHTML = num1;
+  } else if (
+    this.id === 'add' ||
+    this.id === 'subtract' ||
+    this.id === 'multiply' ||
+    this.id === 'divide'
+  ) {
+    operator = this.id;
+    display.innerHTML = this.innerHTML;
+  }
+
+  if (this.id === 'equals') {
+    num1 = Number(num1);
+    num2 = Number(num2);
+    switch (operator) {
+      case 'add':
+        display.innerHTML = add(num1, num2);
+        break;
+      case 'subtract':
+        display.innerHTML = subtract(num1, num2);
+        break;
+      case 'multiply':
+        display.innerHTML = multiply(num1, num2);
+        break;
+      case 'divide':
+        display.innerHTML = divide(num1, num2);
+        break;
+      default:
+        console.log('error');
+    }
+    num1 = '';
+    num2 = '';
+    operator = '';
+  }
 }
+
+buttons.forEach((btn) => btn.addEventListener('click', populate));
+operators.forEach((op) => op.addEventListener('click', populate));
