dab2a3fd454e79852f673ee551e7eb608f5e3f1b
brianpocasangre <brianpocasangre1@gmail.com>
2024-05-20 13:12:16 -0700

    complete one operation at a time function and round long decimal answers

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 let num1 = '';
 let num2 = '';
 let operator = '';
+let result = '';
 
 function add(num1, num2) {
   return num1 + num2;
@@ -20,7 +21,15 @@ const buttons = Array.from(document.querySelectorAll('.pad'));
 const operators = Array.from(document.querySelectorAll('.operate'));
 
 function populate(e) {
-  if (num1 !== '' && operator !== '' && this.id !== 'equals') {
+  if (
+    num1 !== '' &&
+    operator !== '' &&
+    this.id !== 'equals' &&
+    this.id !== 'add' &&
+    this.id !== 'subtract' &&
+    this.id !== 'multiply' &&
+    this.id !== 'divide'
+  ) {
     num2 += this.id;
     display.innerHTML = num2;
   } else if (
@@ -38,8 +47,37 @@ function populate(e) {
     this.id === 'multiply' ||
     this.id === 'divide'
   ) {
-    operator = this.id;
-    display.innerHTML = this.innerHTML;
+    if (num1 !== '' && num2 !== '' && operator !== '') {
+      num1 = Number(num1);
+      num2 = Number(num2);
+      switch (operator) {
+        case 'add':
+          result = add(num1, num2);
+          display.innerHTML = add(num1, num2).toFixed(4);
+          break;
+        case 'subtract':
+          result = subtract(num1, num2);
+          display.innerHTML = subtract(num1, num2).toFixed(4);
+          break;
+        case 'multiply':
+          result = multiply(num1, num2);
+          display.innerHTML = multiply(num1, num2).toFixed(4);
+          break;
+        case 'divide':
+          result = divide(num1, num2);
+          display.innerHTML = divide(num1, num2).toFixed(4);
+          break;
+        default:
+          console.log('error');
+      }
+      num1 = result;
+      num2 = '';
+      operator = this.id;
+      display.innerHTML = result;
+    } else {
+      operator = this.id;
+      display.innerHTML = this.innerHTML;
+    }
   }
 
   if (this.id === 'equals') {
@@ -47,16 +85,16 @@ function populate(e) {
     num2 = Number(num2);
     switch (operator) {
       case 'add':
-        display.innerHTML = add(num1, num2);
+        display.innerHTML = add(num1, num2).toFixed(4);
         break;
       case 'subtract':
-        display.innerHTML = subtract(num1, num2);
+        display.innerHTML = subtract(num1, num2).toFixed(4);
         break;
       case 'multiply':
-        display.innerHTML = multiply(num1, num2);
+        display.innerHTML = multiply(num1, num2).toFixed(4);
         break;
       case 'divide':
-        display.innerHTML = divide(num1, num2);
+        display.innerHTML = divide(num1, num2).toFixed(4);
         break;
       default:
         console.log('error');
@@ -64,6 +102,11 @@ function populate(e) {
     num1 = '';
     num2 = '';
     operator = '';
+  } else if (this.id === 'clear') {
+    num1 = '';
+    num2 = '';
+    operator = '';
+    display.innerHTML = '';
   }
 }
 
